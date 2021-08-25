0024b491ddd94339f2293b4ab0ebb901663ebd5c
zees-dev <dev.786zshan@gmail.com>
2021-08-25 15:37:09 +1200

    mock updated to satisfy interface

--- binary/test/mock.go
+++ binary/test/mock.go
@@ -1,8 +1,6 @@
 package test
 
 import (
-	"time"
-
 	"github.com/portainer/libhelm"
 	"github.com/portainer/libhelm/options"
 	"github.com/portainer/libhelm/release"
@@ -15,6 +13,9 @@ const (
 	MockDataValues = "mock-values"
 )
 
+// helmMockPackageManager is a test package for helm related http handler testing
+// Note: this package currently uses a slice in a way that is not thread safe.
+// Do not use this package for concurrent tests.
 type helmMockPackageManager struct{}
 
 // NewMockHelmBinaryPackageManager initializes a new HelmPackageManager service (a mock instance)
@@ -22,49 +23,41 @@ func NewMockHelmBinaryPackageManager(binaryPath string) libhelm.HelmPackageManag
 	return &helmMockPackageManager{}
 }
 
-type mockChart struct {
-	Name       string    `json:"name"`
-	Namespace  string    `json:"namespace"`
-	Updated    time.Time `json:"updated"`
-	Status     string    `json:"status"`
-	Chart      string    `json:"chart"`
-	AppVersion string    `json:"app_version"`
-}
+var mockCharts = []release.ReleaseElement{}
 
-var mock_charts = []*mockChart{}
-
-func newMockChart(installOpts options.InstallOptions) *mockChart {
-	return &mockChart{
+func newMockReleaseElement(installOpts options.InstallOptions) *release.ReleaseElement {
+	return &release.ReleaseElement{
 		Name:       installOpts.Name,
 		Namespace:  installOpts.Namespace,
-		Updated:    time.Now(),
+		Updated:    "date/time",
 		Status:     "deployed",
-		Chart:      installOpts.Chart + "9.4.2",
-		AppVersion: "1.21.1",
+		Chart:      installOpts.Chart,
+		AppVersion: "1.2.3",
 	}
 }
 
-func mockChartAsRelease(mc *mockChart) *release.Release {
+func newMockRelease(re *release.ReleaseElement) *release.Release {
 	return &release.Release{
-		Name:      mc.Name,
-		Namespace: mc.Namespace,
+		Name:      re.Name,
+		Namespace: re.Namespace,
 	}
 }
 
+// Install a helm chart (not thread safe)
 func (hpm *helmMockPackageManager) Install(installOpts options.InstallOptions) (*release.Release, error) {
 
-	release := newMockChart(installOpts)
+	releaseElement := newMockReleaseElement(installOpts)
 
 	// Enforce only one chart with the same name per namespace
-	for i, rel := range mock_charts {
+	for i, rel := range mockCharts {
 		if rel.Name == installOpts.Name && rel.Namespace == installOpts.Namespace {
-			mock_charts[i] = release
-			return mockChartAsRelease(release), nil
+			mockCharts[i] = *releaseElement
+			return newMockRelease(releaseElement), nil
 		}
 	}
 
-	mock_charts = append(mock_charts, release)
-	return mockChartAsRelease(release), nil
+	mockCharts = append(mockCharts, *releaseElement)
+	return newMockRelease(releaseElement), nil
 }
 
 // Show values/readme/chart etc
@@ -78,4 +71,19 @@ func (hpm *helmMockPackageManager) Show(showOpts options.ShowOptions) ([]byte, e
 		return []byte(MockDataValues), nil
 	}
 	return nil, nil
+}
+
+// Uninstall a helm chart (not thread safe)
+func (hpm *helmMockPackageManager) Uninstall(uninstallOpts options.UninstallOptions) error {
+	for i, rel := range mockCharts {
+		if rel.Name == uninstallOpts.Name && rel.Namespace == uninstallOpts.Namespace {
+			mockCharts = append(mockCharts[:i], mockCharts[i+1:]...)
+		}
+	}
+	return nil
+}
+
+// List a helm chart (not thread safe)
+func (hpm *helmMockPackageManager) List(listOpts options.ListOptions) ([]release.ReleaseElement, error) {
+	return mockCharts, nil
 }
